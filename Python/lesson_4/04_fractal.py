4fcedf7b4491079351591bb6b6fe8735eaa2f332
Alan Latsoyev <alanlatsoyev@gmail.com>
2021-10-06 14:38:53 +0700

    выполнение задния 04 четвертого урока

--- Python/lesson_4/04_fractal.py
+++ Python/lesson_4/04_fractal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-
+sd.resolution = (1300, 700)
 # 1) Написать функцию draw_branches, которая должна рисовать две ветви дерева из начальной точки
 # Функция должна принимать параметры:
 # - точка начала рисования,
@@ -26,6 +26,44 @@ import simple_draw as sd
 
 # можно поиграть -шрифтами- цветами и углами отклонения
 
+# def draw_branches(point, angle, length):
+#     v1 = sd.get_vector(start_point=point, angle=angle-30, length=length, width=3)
+#     v1.draw()
+#     v2 = sd.get_vector(start_point=point, angle=angle+30, length=length, width=3)
+#     v2.draw()
+
+
+# def draw_branches(point, angle, length, delta):
+#     if length < 10:
+#         return
+#     v1 = sd.get_vector(start_point=point, angle=angle-delta, length=length, width=1)
+#     v1.draw()
+#     v2 = sd.get_vector(start_point=point, angle=angle+delta, length=length, width=1)
+#     v2.draw()
+#     next_point_1 = v1.end_point
+#     next_point_2 = v2.end_point
+#     next_angle_1 = angle - delta
+#     next_angle_2 = angle + delta
+#     next_length_1 = length * .75
+#     next_length_2 = length * .75
+#     draw_branches(point=next_point_1, angle=next_angle_1, length=next_length_1, delta=delta)
+#     draw_branches(point=next_point_2, angle=next_angle_2, length=next_length_2, delta=delta)
+#
+#
+# point_0 = sd.get_point(600, 10)
+# draw_branches(point=point_0, angle=90, length=100, delta=30)
+
+
+
+# def branch(point, angle, length, delta):
+#     if length < 1:
+#         return
+#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+#     v1.draw()
+#     next_point = v1.end_point
+#     next_angle = angle - delta
+#     next_length = length * .75
+#     branch(point=next_point, angle=next_angle, length=next_length, delta=delta)
 # TODO здесь ваш код
 
 # 4) Усложненное задание (делать по желанию)
@@ -34,7 +72,31 @@ import simple_draw as sd
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_02.jpg
 
 # Пригодятся функции
-# sd.random_number()
+# delta_angle = sd.random_number(18, 42)
+# delta_length = sd.random_number(.6, .9)
+def draw_branches(point, angle, length):
+    if length < 10:
+        return
+
+    v1 = sd.get_vector(start_point=point, angle=angle-(30 + sd.random_number(-30, 30)/100), length=length, width=1)
+    v1.draw()
+    next_point_1 = v1.end_point
+    next_angle_1 = angle - (30 + 30 * sd.random_number(-40, 40)/100)
+    next_length = length * (.75 + .75 * sd.random_number(-20, 20)/100)
+    draw_branches(point=next_point_1, angle=next_angle_1, length=next_length)
+
+
+    v2 = sd.get_vector(start_point=point, angle=angle+(30 + sd.random_number(-30, 30)/100), length=length, width=1)
+    v2.draw()
+    next_point_2 = v2.end_point
+    next_angle_2 = angle + (30 + 30 * sd.random_number(-40, 40)/100)
+    next_length_2 = length * (.75 + .75 * sd.random_number(-20, 20)/100)
+    draw_branches(point=next_point_2, angle=next_angle_2, length=next_length_2)
+
+
+point_0 = sd.get_point(600, 10)
+draw_branches(point=point_0, angle=90, length=100)
+
 
 sd.pause()
 
