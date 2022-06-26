33046b9648c38b4761264d8c88013f6dcaebcc32
Alan Latsoyev <alanlatsoyev@gmail.com>
2022-06-26 17:52:17 +0700

    completion of task 3 of lesson 9

--- Python/lesson_9/03_files_arrange.py
+++ Python/lesson_9/03_files_arrange.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os, time, shutil
+import shutil
 
 # Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
 # Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
@@ -34,7 +34,33 @@ import os, time, shutil
 # Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import time
+import os
+
+
+class Files_arrange:
+    def __init__(self, path):
+        self.path = os.path.normpath(path)
+
+    def arrange(self, folder_name):
+        for dirpath, dirnames, filenames in os.walk(self.path):
+            for file in filenames:
+                full_file_path = os.path.join(dirpath, file)
+                secs = os.path.getmtime(full_file_path)
+                file_time = time.gmtime(secs)
+                new_path = os.path.join(os.path.dirname(self.path), folder_name, str(file_time[0]), str(file_time[1]))
+                if os.path.exists(new_path):
+                    filepath = os.path.join(dirpath, file)
+                    shutil.copy2(filepath, new_path)
+                else:
+                    os.makedirs(new_path)
+                    filepath = os.path.join(dirpath, file)
+                    shutil.copy2(filepath, new_path)
+
+
+path = 'icons'
+icons = Files_arrange(path=path)
+icons.arrange(folder_name='icons_new')
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
