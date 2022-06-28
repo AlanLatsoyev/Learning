300bfaf5c22863a61fca4e160289b80d71a9649f
Alan Latsoyev <alanlatsoyev@gmail.com>
2022-06-28 19:38:38 +0700

    completion of task 3 of lesson 10

--- Python/lesson_10/03_registration_log.py
+++ Python/lesson_10/03_registration_log.py
@@ -21,5 +21,32 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+class NotNameError(Exception):
+    pass
 
-# TODO здесь ваш код
+
+class NotEmailError(Exception):
+    pass
+
+
+def exam(line):
+    name, email, age = line.split(' ')
+    if name and email and age is None:
+        raise ValueError('НЕ присутсвуют все три поля')
+    if not (name.isalpha()):
+        raise NotNameError('имя введено не верно или содержит цифру')
+    if not ('.' and '@' in email):
+        raise NotEmailError('email введен не верно')
+    if not (10 < int(age) < 99):
+        raise ValueError('возраст не попадает в промежуток от 10 до 99')
+    with open('registrations_good.log', 'a', encoding='utf8') as fr:
+        fr.write(line)
+
+with open('registrations.txt', 'r', encoding='utf8') as ff:
+    for line in ff:
+
+        try:
+            exam(line)
+        except (ValueError, NotNameError, NotEmailError) as exc:
+            with open('registrations_bad.log', 'a', encoding='utf8') as fr:
+                fr.write(f'В строке {line} ошибка вида {exc} ')
